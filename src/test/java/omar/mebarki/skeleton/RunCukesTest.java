edf2cf453f3c3c1bfa28fc88a45b6d55f3cb7812
Omar MEBARKI <omar.mebarki@gmail.com>
2015-03-08 22:28:58 +0100

    cucumber with maven

---
+++ src/test/java/omar/mebarki/skeleton/RunCukesTest.java
@@ -0,0 +1,14 @@
+package omar.mebarki.skeleton;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by Omar MEBARKI on 08/03/15.
+ */
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty"})
+public class RunCukesTest {
+}
